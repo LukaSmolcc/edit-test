6d70444bdcdd8f4cc3205c11369087197899a084
lsmolcic <luka@locastic.com>
2024-04-02 18:00:22 +0200

    added hello

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 export const Home = () => {
   return (
     <div>
-      <p>Home page</p>
+      <p>Home page. Hello!</p>
       <Link to="/about">Link to about page</Link>
     </div>
   );
